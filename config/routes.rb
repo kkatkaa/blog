563b66b887d460f9551970f36b8087285cf99cd7
Kasia <katka36@tlen.pl>
2018-12-02 12:18:45 +0100

    Create find articles in comments controller

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,8 @@ Rails.application.routes.draw do
   # patch 'articles/:id', to: 'articles#update'
   # delete 'articles/:id', to: 'articles#destroy'
 
-  resources :articles
+  resources :articles do
   resources :comments
+  end 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
