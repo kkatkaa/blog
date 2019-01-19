ca3b377c6bfa04a02e951b26d2a85b378efde449
Kasia <katka36@tlen.pl>
2019-01-19 13:18:43 +0100

    Delete table movies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_19_103022) do
+ActiveRecord::Schema.define(version: 2019_01_19_121424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,15 +52,6 @@ ActiveRecord::Schema.define(version: 2019_01_19_103022) do
     t.index ["article_id"], name: "index_likes_on_article_id"
     t.index ["user_id", "article_id"], name: "index_likes_on_user_id_and_article_id", unique: true
     t.index ["user_id"], name: "index_likes_on_user_id"
-  end
-
-  create_table "movies", force: :cascade do |t|
-    t.string "tytuł"
-    t.string "rok_produkcji"
-    t.string "czas_trwania"
-    t.text "opis"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
