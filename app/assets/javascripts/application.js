bbf0d6b8604192c3021809c63404719841e8c77f
Kasia <katka36@tlen.pl>
2019-01-19 15:06:13 +0100

    Add summary button to async fetch data

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,7 +20,33 @@
 
 $(document).ready(function(){
   $('#show-comments-button').click(function(){
-  $('#show-comments-button').text('Hide');
+  $('#show-comments-button').text("Hide");
   $('#users-comments').toggle();
   });
 });
+
+window.addEventListener('load', function() {
+  console.log('Hello from rails');
+
+  var summaryElements = document.querySelectorAll(".summary");
+
+  for(i = 0; i < summaryElements.length; i++) {
+    summaryElements[i].addEventListener("click", function(e) {
+      e.preventDefault();
+
+      var request = new XMLHttpRequest();
+      request.open("GET", this.href + ".json");
+      request.responseType = "json";
+      request.addEventListener("load", function() {
+        var summary = document.getElementById("summary-" + this.response.id)
+        var text = "Comments: " + this.response.comments_count;
+        text += " Likes: " + this.response.likes_count;
+        text += " Views: " + this.response.views_count;
+
+      summary.innerText = text;
+    });
+    this.innerText = "Loading:"
+      request.send();
+    });
+  }
+})
